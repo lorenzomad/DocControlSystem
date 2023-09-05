2e133cbd513aa63d7e6a51272d4a508e4497611f
Lorenzo Maddalena <lorenzo.maddalena1@gmail.com>
2023-09-05 13:58:18 +0000

    cleaned doc

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import Uploader from './components/Uploader.js'
 
 function App() {
   return (
     <div>
-      <Uploader title='File 1'></Uploader>
+      <div className='upload-container'>
+        <Uploader title='File 1'></Uploader>
+        <Uploader title='File 2'></Uploader>
+      </div>
+
+      <button> Generate diff</button>
+      
     </div>
   );
 }
