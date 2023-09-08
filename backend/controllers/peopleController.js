19aa3c8681b2da6353b5c5556cef90807740f61c
Lorenzo Maddalena <lorenzo.maddalena1@gmail.com>
2023-09-08 12:41:04 +0000

    created person create reqeust handler w validation

--- backend/controllers/peopleController.js
+++ backend/controllers/peopleController.js
@@ -13,11 +13,52 @@ exports.person_detail = asyncHandler(async(req,res,next ) => {
 })
 
 // create person post
-exports.person_create_post = asyncHandler( async (req,res,next) => {
-    body("name")
-    
-    res.send("TBD")
+exports.person_create_post = [
+    // data validation
+    body("firstName")
+    .trim()
+    .isLength({min:3})
+    .escape(),
+    body("LastName")
+    .trim()
+    .isLength({min:3})
+    .escape(),
+    body("role")
+    .trim()
+    .isLength({min:3})
+    .escape(),
+
+    // handle the request
+    asyncHandler( async (req,res,next) => {
+        const errors = validationResult(req)
+        const person = new People({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            role: req.body.role
+        })
+
+        if(!errors.isEmpty()) {
+            //data contains vlaidation errors
+            res.send("validation of the input failed");
+            return 
+        } else {
+            //data validation passed
+            const personExists = await People.findOne({ 
+                firstName: req.body.firstName,
+                lastName: req.body.lastName 
+            }).exec();
+
+            if (personExists) {
+                // the person already exists, redirect
+                res.redirect(personExists.url)
+            } else {
+                // save the person
+                await person.save()
+                res.redirect(person.url)
+            }
+        }
 })
+]
 
 //delete person post 
 exports.person_delete_post = asyncHandler( async (req,res,next) => {
